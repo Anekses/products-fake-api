4063a1612862124bfaa83e40a21795042fdaf8f6
Adrian Rydzynski <adrian.rydzynski@allegro.pl>
2019-05-20 19:52:15 +0200

    Cors

--- src/main/java/pl/allegrotech/umk/productsfakeapi/ProductsApi.java
+++ src/main/java/pl/allegrotech/umk/productsfakeapi/ProductsApi.java
@@ -1,5 +1,6 @@
 package pl.allegrotech.umk.productsfakeapi;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 public class ProductsApi {
 
     @GetMapping
+    @CrossOrigin
     List<ProductDto> products() {
         return Collections.unmodifiableList(
                 Arrays.asList(
